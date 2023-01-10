dc6e4dfb06e66f80fae286ffd2f56d2271957eb7
Kerry Mahne <kerry.mahne@gmail.com>
2023-01-10 16:05:52 +0100

    retina url apdejt

--- app/components/nahajalisce-marker/nahajalisce-marker.client.tsx
+++ app/components/nahajalisce-marker/nahajalisce-marker.client.tsx
@@ -6,7 +6,8 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 
 const customIcon = new Leaflet.Icon.Default({
-  iconRetinaUrl: 'https://i.imgur.com/UxAiBHa.png',
+  iconRetinaUrl:
+    'https://upload.wikimedia.org/wikipedia/commons/7/70/%D0%9F%D0%BE%D0%B4%D1%83%D1%88%D0%B5%D1%87%D0%BA%D0%B0_%D1%81%D1%82%D1%83%D0%BF%D0%BD%D0%B8.png',
   iconUrl:
     'https://upload.wikimedia.org/wikipedia/commons/7/70/%D0%9F%D0%BE%D0%B4%D1%83%D1%88%D0%B5%D1%87%D0%BA%D0%B0_%D1%81%D1%82%D1%83%D0%BF%D0%BD%D0%B8.png',
   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
