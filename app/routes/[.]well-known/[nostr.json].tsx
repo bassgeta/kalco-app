8d4702c29b5bff4aedef343d86cf5b7adec7bc2c
Kerry Mahne <kerry.mahne@gmail.com>
2023-02-03 14:24:36 +0100

    sam senkrat se triggerej

--- app/routes/[.]well-known/[nostr.json].tsx
+++ app/routes/[.]well-known/[nostr.json].tsx
@@ -2,6 +2,5 @@ import { json } from '@remix-run/node'; // or cloudflare/deno
 import names from './names.json';
 
 export const loader = async () => {
-  // So you can write this:
   return json(names);
 };
